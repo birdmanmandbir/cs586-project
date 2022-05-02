549bac7b08c1355697fe1f4b7e1c19bcbd225e14
Nneil_admin <1522674529@qq.com>
2022-05-02 18:48:49 +0800

    account1 driver

--- src/views/Account1.tsx
+++ src/views/Account1.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Input } from 'antd';
+import { Button, Input } from 'antd';
 import './Account.css'
 import { Account1 } from "../backend/Account1";
 import { A1_ConcreteFactory } from "../backend/factory/A1_ConcreteFactory";
@@ -13,11 +13,17 @@ export default function Account1Panel() {
   const [account1, setAccount1] = useState(new Account1(m, factory.createDataStore()))
   const [pin, setPin] = useState(0);
   const [id, setId] = useState(0);
+  const [balance, setBalance] = useState(0);
   return (
     <div className="account1-panel">
+      <div className="header">Current State: {m.s.constructor.name}</div>
+      <span>Pin: </span>
       <Input placeholder="input the pin" value={pin} onChange={(e) => { setPin(Number(e.target.value)) }}></Input>
+      <span>Id: </span>
       <Input placeholder="input user identification" value={id} onChange={(e) => { setId(Number(e.target.value)) }}></Input>
-      <Input placeholder="input the balance"></Input>
+      <span>Balance: </span>
+      <Input placeholder="input the balance" value={balance} onChange={(e) => setBalance(Number(e.target.value))}></Input>
+      <Button onClick={() => account1.open(pin, id, balance)}>open</Button>
     </div>
   )
 }
