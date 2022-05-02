394ba02cfab63b7cb41cd0e51009b4acb156f93c
Nneil_admin <1522674529@qq.com>
2022-05-02 19:08:08 +0800

    setState

--- src/views/Account1.tsx
+++ src/views/Account1.tsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { Button, Input } from 'antd';
-import './Account.css'
-import { Account1 } from "../backend/Account1";
-import { A1_ConcreteFactory } from "../backend/factory/A1_ConcreteFactory";
-import { MDAEFSM } from "../backend/MdaEfsm";
-import { OP } from "../backend/OP";
+import './Account.css';
+import { Account1 } from '../backend/Account1';
+import { A1_ConcreteFactory } from '../backend/factory/A1_ConcreteFactory';
+import { MDAEFSM } from '../backend/MdaEfsm';
+import { OP } from '../backend/OP';
+import { State } from '../backend/state/State';
 
 export default function Account1Panel() {
   const [factory, setFactory] = useState(new A1_ConcreteFactory());
-  const [op, setOP] = useState(new OP(factory.createDataStore(), factory))
-  const [m, setM] = useState(new MDAEFSM(op))
-  const [s, setState] = useState(m.s)
-  m.setState = setState;
-  const [account1, setAccount1] = useState(new Account1(m, factory.createDataStore()))
+  const [op, setOP] = useState(new OP(factory.createDataStore(), factory));
+  const [m, setM] = useState(new MDAEFSM(op));
+  const [s, setState] = useState(m.s);
+  m.setState = (state: State) => {
+    m.s = state;
+    setState(state);
+    console.log('state change to: ', s.constructor.name)
+  };
+  const [account1, setAccount1] = useState(new Account1(m, factory.createDataStore()));
   const [pin, setPin] = useState(0);
   const [id, setId] = useState(0);
   const [balance, setBalance] = useState(0);
@@ -20,12 +25,28 @@ export default function Account1Panel() {
     <div className="account1-panel">
       <div className="header">Current State: {s.constructor.name}</div>
       <span>Pin: </span>
-      <Input placeholder="input the pin" value={pin} onChange={(e) => { setPin(Number(e.target.value)) }}></Input>
+      <Input
+        placeholder="input the pin"
+        value={pin}
+        onChange={(e) => {
+          setPin(Number(e.target.value));
+        }}
+      ></Input>
       <span>Id: </span>
-      <Input placeholder="input user identification" value={id} onChange={(e) => { setId(Number(e.target.value)) }}></Input>
+      <Input
+        placeholder="input user identification"
+        value={id}
+        onChange={(e) => {
+          setId(Number(e.target.value));
+        }}
+      ></Input>
       <span>Balance: </span>
-      <Input placeholder="input the balance" value={balance} onChange={(e) => setBalance(Number(e.target.value))}></Input>
+      <Input
+        placeholder="input the balance"
+        value={balance}
+        onChange={(e) => setBalance(Number(e.target.value))}
+      ></Input>
       <Button onClick={() => account1.open(pin, id, balance)}>open</Button>
     </div>
-  )
+  );
 }
