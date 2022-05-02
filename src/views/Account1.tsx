997e03aed1739ec78ea0092e06c16b5cce8faf90
Nneil_admin <1522674529@qq.com>
2022-05-02 19:19:33 +0800

    account1 driver ui

--- src/views/Account1.tsx
+++ src/views/Account1.tsx
@@ -15,38 +15,60 @@ export default function Account1Panel() {
   m.setState = (state: State) => {
     m.s = state;
     setState(state);
-    console.log('state change to: ', s.constructor.name)
+    console.log('state change to: ', s.constructor.name);
   };
   const [account1, setAccount1] = useState(new Account1(m, factory.createDataStore()));
-  const [pin, setPin] = useState(0);
-  const [id, setId] = useState(0);
-  const [balance, setBalance] = useState(0);
+  const [p, setP] = useState(0);
+  const [y, setY] = useState(0);
+  const [a, setA] = useState(0);
+  const [x, setX] = useState(0);
+  const [d, setD] = useState(0);
+  const [w, setW] = useState(0);
   return (
     <div className="account1-panel">
       <div className="header">Current State: {s.constructor.name}</div>
-      <span>Pin: </span>
-      <Input
-        placeholder="input the pin"
-        value={pin}
-        onChange={(e) => {
-          setPin(Number(e.target.value));
-        }}
-      ></Input>
-      <span>Id: </span>
-      <Input
-        placeholder="input user identification"
-        value={id}
-        onChange={(e) => {
-          setId(Number(e.target.value));
-        }}
-      ></Input>
-      <span>Balance: </span>
-      <Input
-        placeholder="input the balance"
-        value={balance}
-        onChange={(e) => setBalance(Number(e.target.value))}
-      ></Input>
-      <Button onClick={() => account1.open(pin, id, balance)}>open</Button>
+      <div className="operation">
+        <span>Pin: </span>
+        <Input
+          placeholder="input the pin"
+          value={p}
+          onChange={(e) => {
+            setP(Number(e.target.value));
+          }}
+        ></Input>
+        <span>Id: </span>
+        <Input
+          placeholder="input user identification"
+          value={y}
+          onChange={(e) => {
+            setY(Number(e.target.value));
+          }}
+        ></Input>
+        <span>Balance: </span>
+        <Input placeholder="input the balance" value={a} onChange={(e) => setA(Number(e.target.value))}></Input>
+        <Button onClick={() => account1.open(p, y, a)}>open</Button>
+      </div>
+      <div className="operation">
+        <span>Provides pin: </span>
+        <Input value={x} onChange={(e) => setX(Number(e.target.value))}></Input>
+        <Button onClick={() => account1.pin(x)}>pin</Button>
+        <Button onClick={() => account1.lock(x)}>lock</Button>
+        <Button onClick={() => account1.unlock(x)}>unlock</Button>
+      </div>
+      <div className="operation">
+        <span>Deposite: </span>
+        <Input value={d} onChange={(e) => setD(Number(e.target.value))}></Input>
+        <Button onClick={() => account1.deposit(d)}>deposit</Button>
+      </div>
+      <div className="operation">
+        <span>Withdraw: </span>
+        <Input value={w} onChange={(e) => setW(Number(e.target.value))}></Input>
+        <Button onClick={() => account1.withdraw(w)}>withdraw</Button>
+      </div>
+      <div className="operation">
+        <Button onClick={() => account1.balance()}>balance</Button>
+        <Button onClick={() => account1.logout()}>logout</Button>
+      </div>
     </div>
   );
 }
